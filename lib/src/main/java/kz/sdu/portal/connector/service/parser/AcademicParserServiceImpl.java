dd061336bfb66d363e9d4bc755cd7d1f805d20d6
Sanzhar Zhanibekov <semid46@gmail.com>
2023-09-13 21:34:30 +0600

    Refactored AcademicParserServiceImpl for better type safety
    Imported StringUtil class in AcademicParserServiceImpl and created a private getEctsFromData method, which checks whether given ECTS values are of the expected type before parsing. This ensures the data integrity and reduces the risk of a ClassCastException in the parsing process. Also, added a isNumber method in StringUtil for verifying if a string value is numeric.

--- lib/src/main/java/kz/sdu/portal/connector/service/parser/AcademicParserServiceImpl.java
+++ lib/src/main/java/kz/sdu/portal/connector/service/parser/AcademicParserServiceImpl.java
@@ -1,6 +1,7 @@
 package kz.sdu.portal.connector.service.parser;
 
 import kz.sdu.portal.connector.dto.portal.StudentAcademic;
+import kz.sdu.portal.connector.util.StringUtil;
 
 import java.util.Map;
 
@@ -13,12 +14,30 @@ public class AcademicParserServiceImpl implements PortalResponseDataParserServic
   @Override
   public StudentAcademic parseResponse(String studentId, Map<String, Object> response) {
     StudentAcademic studentAcademic = new StudentAcademic();
-
     studentAcademic.setStudentId(studentId);
-    studentAcademic.setTotalEcts(Integer.parseInt((String) response.get(TOTAL_ECTS_NAME)));
-    studentAcademic.setTotalEctsForSemester(Integer.parseInt((String) response.get(TOTAL_ECTS_FOR_SEMESTER_NAME)));
-    studentAcademic.setTotalEctsRetake((Integer)response.get(TOTAL_ECTS_RETAKE_NAME));
-    studentAcademic.setTotalEctsCompleted(Integer.parseInt((String) response.get(TOTAL_ECTS_COMPLETED_NAME)));
+    studentAcademic.setTotalEcts(getEctsFromData(response, TOTAL_ECTS_NAME));
+    studentAcademic.setTotalEctsForSemester(getEctsFromData(response, TOTAL_ECTS_FOR_SEMESTER_NAME));
+    studentAcademic.setTotalEctsRetake(getEctsFromData(response, TOTAL_ECTS_RETAKE_NAME));
+    studentAcademic.setTotalEctsCompleted(getEctsFromData(response, TOTAL_ECTS_COMPLETED_NAME));
     return studentAcademic;
+  }
+
+  private static Integer getEctsFromData(Map<String, Object> response, final String key) {
+    if (response.get(key) instanceof String) {
+      String v = (String) response.get(key);
+      if (StringUtil.isNumber(v)) {
+        return Integer.parseInt(v);
+      } else {
+        String e = String.format("Can't cast String '%S' to Integer, cause values is %s",
+            key, v);
+        throw new ClassCastException(e);
+      }
+    } else if (response.get(key) instanceof Integer) {
+      return (Integer) response.get(key);
+    } else {
+      String e = String.format("Can't cast value from '%S'. Value is: '%s'",
+          key, response.get(key));
+      throw new ClassCastException(e);
+    }
   }
 }
